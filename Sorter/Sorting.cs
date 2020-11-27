0beb8a3082abc6f94583426a14f5a2f3013a701d
Alan Peña <lanazirot@gmail.com>
2020-11-27 13:37:11 -0600

    Bubble Sort deleted and Radix Sort w/ counting sort implemented

--- Sorter/Sorting.cs
+++ Sorter/Sorting.cs
@@ -1,156 +1,124 @@
-﻿using System;
-using System.Linq;
-using DS;
+﻿using System.Linq;
 using System.Collections.Generic;
 
 namespace Sorter {
     /// <summary>
-    /// Radixable is an Radix Sort needed interface for implement this sorting
-    /// with objects. Object needs an integer Primary Key; this PK can be asigned
-    /// as any of class Properties / Attributes. Make sure implement correctly this
-    /// interface for a great Radix Sort.
+    /// Permite utilizar Radix Sort cuando se implementa en el diseño de una clase.
     /// </summary>
     public interface IRadixable {
         /// <summary>
-        /// Function to asign and return the class' primary key
+        /// Se debe asignar el atributo por el cual se desea ordenar.
         /// </summary>
-        /// <returns>Primary key from class (any int data)</returns>
-        int GetPK();
+        /// <returns>Atributo para ordenar (entero)</returns>
+        int ObtenerAtributoParaOrdenar();
     }
 
 
 
     public class Sorting {
 
-        private static void Swap<T>(ref T a, ref T b) {
-            T auxiliar = a;
-            a = b;
-            b = auxiliar;
-        }
+ 
+        private static int ObtenerMaximoDeObjetoRadixable<T>(T[] arreglo) where T : IRadixable {
+            int mayorAhora = arreglo[0].ObtenerAtributoParaOrdenar();
 
-        private static void BubbleSortWithSignal<T>(ref T[] arreglo) where T : IComparable<T> {
-            bool blnMovimiento;
-            for (var i = 0; i < arreglo.Length; i++) {
-                blnMovimiento = false;
-                for (var j = 0; j < arreglo.Length - 1; j++) {
-                    if (arreglo[j].CompareTo(arreglo[j + 1]) == 1) {
-                        Swap(ref arreglo[j], ref arreglo[j + 1]);
-                        blnMovimiento = true;
-                    }
-                }
-                if (!blnMovimiento) break;
-            }
-        }
-
-        private static void BubbleSortLeft<T>( ref T[] arreglo) where T : IComparable<T> {
-            for (var i = 0; i < arreglo.Length; i++) {
-                for (var j = 0; j < arreglo.Length - i - 1; j++) {
-                    if (arreglo[j+1].CompareTo(arreglo[j]) == 1) {
-                        Swap(ref arreglo[j+1], ref arreglo[j]);
-                    }
-                }
-            }
-        }
-
-        private static void BubbleSortRight<T>(ref T[] arreglo) where T : IComparable<T> {
-            for (var i = 0; i < arreglo.Length; i++) {
-                for (var j = 0; j < arreglo.Length - i - 1; j++) {
-                    if (arreglo[j].CompareTo(arreglo[j+1]) == 1) {
-                        Swap(ref arreglo[j], ref arreglo[j+1]);
-                    }
-                }
-            }
-        }
-
-        public static void BubbleSort<T>(ref T[] arreglo, BubbleSortingType bubbleSortingType 
-            = BubbleSortingType.Signal) where T : IComparable<T> {
-            switch (bubbleSortingType) {
-                case BubbleSortingType.Signal:
-                    BubbleSortWithSignal(ref arreglo);
-                    break;
-                case BubbleSortingType.Left:
-                    BubbleSortLeft(ref arreglo);
-                    break;
-                case BubbleSortingType.Right:
-                    BubbleSortRight(ref arreglo);
-                    break;
-            }
-        }
-
-
-        private static int GetMaxFromRadixable<T>(T[] arreglo) where T : IRadixable {
-            int maxNow = arreglo[0].GetPK();
-
-            for(int i = 1; i < arreglo.Length; i++) {
-                if (arreglo[i].GetPK() > maxNow) {
-                    maxNow = arreglo[i].GetPK();
+            for (int i = 1; i < arreglo.Length; i++) {
+                if (arreglo[i].ObtenerAtributoParaOrdenar() > mayorAhora) {
+                    mayorAhora = arreglo[i].ObtenerAtributoParaOrdenar();
                 }
             }
 
-            return maxNow;
+            return mayorAhora;
         }
-
-        public static void RadixSortObject<T>(ref T[] arreglo) where T : IRadixable{
-            //Declaro 10 cubetas
-            LinkedList<T>[] buckets = new LinkedList<T>[10];
+        /// <summary>
+        /// Radix Sort con objetos que implementen la interface IRadixable.
+        /// </summary>
+        /// <typeparam name="T">Tipo que se desea ordenar</typeparam>
+        /// <see cref="RadixSort(ref int[])"/>
+        /// <param name="arreglo">Arreglo de objetos de tipo T</param>
+        public static void RadixSort<T>(ref T[] arreglo) where T : IRadixable {
+            Queue<T>[] cubetas = new Queue<T>[10];
 
             for (var i = 0; i < 10; i++)
-                //Creo 10 cubetas
-                buckets[i] = new LinkedList<T>();
+                cubetas[i] = new Queue<T>();
 
-            int intRadix; //Me dice en que cubeta me voy a ubicar
-            int intPosition = 1; //Me dice la posicion del digito (unidades, decenas, centenas, miles)
+            int intDivision;
+            int intPosicion = 1;
 
-            int intMaxFromArray = GetMaxFromRadixable(arreglo); // arreglo.Max(); //El mayor elemento del arreglo
-            while (intMaxFromArray / intPosition > 0) {
+            int intMaximoDelArreglo = ObtenerMaximoDeObjetoRadixable(arreglo); 
+            while (intMaximoDelArreglo / intPosicion > 0) {
                 foreach (var intElement in arreglo) {
-                    intRadix = intElement.GetPK() / intPosition;
-                    buckets[intRadix % 10].AddLast(intElement);
+                    intDivision = intElement.ObtenerAtributoParaOrdenar() / intPosicion;
+                    cubetas[intDivision % 10].Enqueue(intElement);
                 }
-                for (int i = 0, a = 0; i < 10; i++) {
-                    foreach (var j in buckets[i]) {
-                        arreglo[a++] = j;
+                for (int i = 0, posicionEnArreglo = 0; i < 10; i++) {
+                    while (cubetas[i].Count != 0) {
+                        arreglo[posicionEnArreglo++] = cubetas[i].Dequeue();
                     }
-                    buckets[i].Clear();
                 }
-                intPosition *= 10;
+                intPosicion *= 10;
             }
-
         }
 
 
-
-        public static void RadixSort(ref int[] arreglo)  {
-            //Declaro 10 cubetas
-            ListaSimpleOrdenada<int>[] buckets = new ListaSimpleOrdenada<int>[10];
+        /// <summary>
+        /// Radix Sort de un arreglo de numeros enteros.
+        /// </summary>
+        /// <param name="arreglo">Arreglo de enteros</param>
+        public static void RadixSortDS(ref int[] arreglo) {
+            Queue<int>[] cubetas = new Queue<int>[10];
 
             for (var i = 0; i < 10; i++)
-                //Creo 10 cubetas
-                buckets[i] = new ListaSimpleOrdenada<int>(true, false);
+                cubetas[i] = new Queue<int>();
 
-            int intRadix; //Me dice en que cubeta me voy a ubicar
-            int intPosition = 1; //Me dice la posicion del digito (unidades, decenas, centenas, miles)
+            int intDivision;
+            int intPosicion = 1; 
 
-            int intMaxFromArray = arreglo.Max(); //El mayor elemento del arreglo
-            while (intMaxFromArray/intPosition > 0) {
-                foreach(var intElement in arreglo) {
-                    intRadix = intElement / intPosition;
-                    buckets[intRadix % 10].Agregar(intElement);
+            int intMaximoDelArreglo = arreglo.Max();
+            while (intMaximoDelArreglo / intPosicion > 0) {
+                foreach (var intElemento in arreglo) {
+                    intDivision = intElemento / intPosicion;
+                    cubetas[intDivision % 10].Enqueue(intElemento);
                 }
-                for(int i = 0, a = 0; i < 10; i++) {
-                    foreach(var j in buckets[i]) {
-                        arreglo[a++] = j;
+                for (int i = 0, posicionEnArreglo = 0; i < 10; i++) {
+                    while (cubetas[i].Count != 0) {
+                        arreglo[posicionEnArreglo++] = cubetas[i].Dequeue();
                     }
-                    buckets[i].Limpiar();
                 }
-                intPosition *= 10;
+                intPosicion *= 10;
             }
-
         }
 
-        public enum BubbleSortingType {
-            Left, Right, Signal
+        private static int ObtenerMaximoEnteroArreglo(int[] arreglo) {
+            int intMaximo = arreglo[0];
+            for (int i = 1; i < arreglo.Length; i++)
+                if (arreglo[i] > intMaximo)
+                    intMaximo = arreglo[i];
+            return intMaximo;
         }
 
+        private static void CountingSort(int[] arreglo, int exp) {
+            int[] arregloDeSalida = new int[arreglo.Length];
+            int[] cubetas = new int[10];
+            for (int i = 0; i < 10; i++)
+                cubetas[i] = 0;
+            for (int i = 0; i < arreglo.Length; i++)
+                cubetas[(arreglo[i] / exp) % 10]++;
+            for (int i = 1; i < 10; i++)
+                cubetas[i] += cubetas[i - 1];
+            for (int i = arreglo.Length - 1; i >= 0; i--) {
+                arregloDeSalida[cubetas[(arreglo[i] / exp) % 10] - 1] = arreglo[i];
+                cubetas[(arreglo[i] / exp) % 10]--;
+            }
+            for (int i = 0; i < arreglo.Length; i++)
+                arreglo[i] = arregloDeSalida[i];
+        }
+
+        public static void RadixSort(ref int[] arreglo) {
+            int intMaximo = ObtenerMaximoEnteroArreglo(arreglo);
+            for(int i = 1; intMaximo/i>0; i *= 10) {
+                CountingSort(arreglo,  i);
+            }
+        }
     }
+
 }
